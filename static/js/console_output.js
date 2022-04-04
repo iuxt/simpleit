50e82e787e3bed29091beebde35d9328de89702f
zhanglikun <iuxt@qq.com>
2022-04-04 23:07:57 +0800

    first commit

---
+++ static/js/console_output.js
@@ -0,0 +1,10 @@
+/* 控制台输出 */
+if(window.console) {
+	var cons = console; 
+	if(cons) { 
+		cons.group("O(∩_∩)O哈喽！");
+		cons.log("%c杂烩饭", "background-image: linear-gradient(#f39800, #f08300, #eb6238);font-size: 4rem;");
+		cons.log("看代码的同学，加个微信好友吧！https://zahui.fan/about/")
+		cons.groupEnd();
+	}
+}
